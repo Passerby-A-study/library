a2a2614c60bbd86c5c3b7c0a123bfb3414c1bdea
Passerby_A <780551528@qq.com>
2025-03-04 21:31:06 +0800

    第一版（到借书那里没思路了）

--- library.h
+++ library.h
@@ -1,21 +1,39 @@
 #pragma once
 #include<string>
+class Book;
 typedef	std::string string;
-class Person {
+class Person :public Get {
 	string P_NAME;
 	string P_ID;
 public:
-	Person(const string& name="Null", const string& id="00000") :P_NAME(name), P_ID(id) {}
-	Person(string& name, string& id);
-	virtual ~Person()=0 {}
+	Person();
+	virtual void GetName();
+	virtual void GetId();
 };
 
 class Borrow {
 public:
-	virtual void markBorrow(Borrow& b) = 0 ;
-	virtual void markReturn(Borrow& b) = 0 ;
+	virtual void markBorrow() = 0 ;
+	virtual void markReturn() = 0 ;
+};
+class Get {
+public:
+	virtual void GetName() = 0;
+	virtual void GetId() = 0;
 };
 
+class Admin :public Person {
+public:
+	Admin() :Person() {}
+	void AddBook(Book&b);
+};
+class User :public Person {
+public:
+	User() :Person() {}
+	void BorrowBook(Book& b);
+	void ReturnBook(Book& b);
+
+};
 class Book :public Borrow{
 private:
 	string B_NAME;
@@ -27,12 +45,17 @@ private:
 	//int BorrowMonth;
 	//int BorrowDay;
 public:
-	Book(const string& bn="None", const string& bid="!0000",bool ib=false/*, int py = 0, const string& a = "None",
-		int y=0, int m=0, int d=0*/) :B_NAME(bn), B_ID(bid),isBorrowed(ib)/*, PublicYear(py),
-		Author(a), BorrowYear(y), BorrowMonth(m), BorrowDay(d)*/ {}
-	Book(string& bn, string& bid, bool ib/*, int py, string& a,
-		int y, int m, int d*/) ;
-	virtual void markBorrow(Borrow& b) ;
-	virtual void markReturn(Borrow& b) ;
+	//Book(const string& bn="None", const string& bid="!0000",bool ib=false/*, int py = 0, const string& a = "None",
+	//	int y=0, int m=0, int d=0*/) :B_NAME(bn), B_ID(bid),isBorrowed(ib)/*, PublicYear(py),
+	//	Author(a), BorrowYear(y), BorrowMonth(m), BorrowDay(d)*/ {}
+	explicit Book(bool ib = false/*, int py, string& a,int y, int m, int d*/) ;
+	virtual void markBorrow() ;
+	virtual void markReturn() ;
+	virtual void GetName();
+	virtual void GetId();
+	bool ShowBorrow() { return isBorrowed; }
+	friend void ShowAll(Book& b);
+	friend void Admin::AddBook(Book& b);
 };
-
+void markBorrow(Borrow& b);
+void markReturn(Borrow& b);
