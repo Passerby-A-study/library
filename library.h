0faeb1dcb6bbb6eccf7b238ef2d11c96434737f5
Passerby_A <780551528@qq.com>
2025-03-04 22:50:06 +0800

    理清思路后重新写，这是第一份

--- library.h
+++ library.h
@@ -1,61 +1,46 @@
 #pragma once
 #include<string>
+#include<vector>
+using std::string;
+using std::vector;
 class Book;
-typedef	std::string string;
-class Person :public Get {
-	string P_NAME;
-	string P_ID;
+
+//============Library=================
+
+class Library {
+private:
+	vector<Book> BookShelter;
 public:
-	Person();
-	virtual void GetName();
-	virtual void GetId();
+	void Add(Book& b);
 };
 
-class Borrow {
-public:
-	virtual void markBorrow() = 0 ;
-	virtual void markReturn() = 0 ;
-};
-class Get {
-public:
-	virtual void GetName() = 0;
-	virtual void GetId() = 0;
-};
 
-class Admin :public Person {
+//=============Book====================
+class Book {
+private:
+	string BookName;
+	string BookID;
 public:
-	Admin() :Person() {}
-	void AddBook(Book&b);
+	Book(string&name,string&id):BookName(name),BookID(id){}
 };
+//=============Person==================
+class Person {
+private:
+	string UserName;
+	string UserID;
+public:
+	Person(string& name, string& id) :UserName(name), UserID(id) {}
+};
+//=============Admin===================
+class Admin:public Person {
+public:
+	Admin();
+	void AddBook();
+	void Show();
+};
+//=============Client==================
 class User :public Person {
 public:
-	User() :Person() {}
-	void BorrowBook(Book& b);
-	void ReturnBook(Book& b);
-
+	User();
+	void BorrowBook();
 };
-class Book :public Borrow{
-private:
-	string B_NAME;
-	string B_ID;
-	bool isBorrowed;
-	//int PublicYear;
-	//string Author;
-	//int BorrowYear;
-	//int BorrowMonth;
-	//int BorrowDay;
-public:
-	//Book(const string& bn="None", const string& bid="!0000",bool ib=false/*, int py = 0, const string& a = "None",
-	//	int y=0, int m=0, int d=0*/) :B_NAME(bn), B_ID(bid),isBorrowed(ib)/*, PublicYear(py),
-	//	Author(a), BorrowYear(y), BorrowMonth(m), BorrowDay(d)*/ {}
-	explicit Book(bool ib = false/*, int py, string& a,int y, int m, int d*/) ;
-	virtual void markBorrow() ;
-	virtual void markReturn() ;
-	virtual void GetName();
-	virtual void GetId();
-	bool ShowBorrow() { return isBorrowed; }
-	friend void ShowAll(Book& b);
-	friend void Admin::AddBook(Book& b);
-};
-void markBorrow(Borrow& b);
-void markReturn(Borrow& b);
--- library1.cpp
+++ library1.cpp
@@ -1,57 +1,8 @@
 #include"library.h"
-#include<iostream>
-using std::cin;
-using std::cout;
-using std::endl;
 
-Person::Person() {
-	GetName();
-	GetId();
-	cout << endl << "Welcome "<<P_NAME<<" !" << endl;
-}
-void Person::GetName() {
-	cout << "pls Enter Your Name : ";
-	cin >> P_NAME;
-}
-void Person::GetId() {
-	cout << endl << "pls Enter Your Id : ";
-	cin >> P_ID;
-}
-//------------------------------------------
-void Book::GetName() {
-	cout << "pls Enter The Name of Book : ";
-	cin >> B_NAME;
-}
-void Book::GetId() {
-	cout << endl << "pls Enter The Id of Book : ";
-	cin >> B_ID;
-}
-Book::Book(bool ib) :isBorrowed(ib) {
-	GetName();
-	GetId();
-	cout << endl << B_NAME << " is Loaded! " << endl;
-}
-void Book::markBorrow() {
-	isBorrowed = true;
-}
-void Book::markReturn() {
-	isBorrowed = false;
-}
-//-------------------------------------
-void Admin::AddBook(Book& b) {
 
-}
-//-------------------------------------
-void User::BorrowBook(Book& b) {
 
-}
-void User::ReturnBook(Book& b) {
+int main() {
 
-}
-//-------------------------------------
-void markBorrow(Borrow& b) {
-	b.markBorrow();
-}
-void markReturn(Borrow& b) {
-	b.markReturn();
+	return 0;
 }
