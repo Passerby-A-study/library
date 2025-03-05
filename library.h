300750b1ed44db194e1838e25f39bca3c8185603
Passerby_A <780551528@qq.com>
2025-03-05 22:51:40 +0800

    完成Admin类和方法的设计 2025-03-05   22：52

--- library.h
+++ library.h
@@ -1,27 +1,44 @@
 #pragma once
 #include<string>
 #include<vector>
+#include<iostream>
 using std::string;
 using std::vector;
+using std::cout;
+using std::cin;
+using std::endl;
 class Book;
-
+class Admin;
+class User;
 //============Library=================
 
 class Library {
 private:
-	vector<Book> BookShelter;
+	vector<Book*> BookShelter;
+	vector<Admin*> LibAdmin;
+	vector<User*> LibUser;
 public:
-	void Add(Book& b);
+	void Add(Book* b);
+	void Show();
 };
-
+//=============Get=====================
+class Get {
+public:
+	virtual string GetName() = 0;
+	virtual string GetID() = 0;
+};
 
 //=============Book====================
 class Book {
 private:
 	string BookName;
 	string BookID;
+	bool isBorrow;
 public:
-	Book(string&name,string&id):BookName(name),BookID(id){}
+	Book(string&name,string&id,bool ib=false):BookName(name),BookID(id),isBorrow(ib){}
+	virtual string GetName() { return BookName; }
+	virtual string GetID() { return BookID; };
+	virtual string GetStatus() { return BookID; };
 };
 //=============Person==================
 class Person {
@@ -30,13 +47,14 @@ private:
 	string UserID;
 public:
 	Person(string& name, string& id) :UserName(name), UserID(id) {}
+	Person(){}
 };
 //=============Admin===================
 class Admin:public Person {
 public:
-	Admin();
-	void AddBook();
-	void Show();
+	Admin(string& name, string& id) :Person(name,id) {}
+	void AddBook(Library& Li);
+	void Show(Library& Li);
 };
 //=============Client==================
 class User :public Person {
--- library1.cpp
+++ library1.cpp
@@ -1,8 +1,30 @@
 #include"library.h"
 
-
-
-int main() {
-
-	return 0;
+//=============Admin===================
+void Admin::AddBook(Library& Li) {
+	string name;
+	string id;
+	cout << "Pls Enter the Name of Book: " << endl;
+	cin >> name;
+	cout << "Pls Enter the ID of Book: " << endl;
+	cin >> id;
+	Book* p = new Book(name, id);
+	Li.Add(p);
+}
+void Admin::Show(Library & Li) {
+	cout << "BookShelter: " << endl;
+	cout << "Book_Name" << "        " << "Book_ID"<<"          " << "Status" << endl;
+	Li.Show();
+}
+string GetName();
+string GetID();
+//===============Library=================
+void Library::Add(Book* b) {
+	BookShelter.push_back(b);
+	delete b;
+}
+void Library::Show() {
+	for (auto book : BookShelter) {
+		cout << book->GetName() << "       " << book->GetID() << "         " << book->GetStatus() << endl;
+	}
 }
